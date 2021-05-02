c541e65740daf0b9a727d551202ca483caa13241
VictorHCH <vcovarrubias@itparral.edu.mx>
2021-05-02 12:17:02 -0600

    Update main.py

--- Carrera/main.py
+++ Carrera/main.py
@@ -1,9 +1,8 @@
 import sys
-import time
-from PyQt5.QtCore import QObject, QRunnable, QThreadPool, QTimer, pyqtSignal, pyqtSlot
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QVBoxLayout, QLCDNumber
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel
 
 
+<<<<<<< HEAD
 class WorkerSignal(QObject):
     """ Define las señales disponibles desde la ejecucion de la Tarea  (hilo)"""
 
@@ -11,43 +10,13 @@ class WorkerSignal(QObject):
 
 
 class Worker(QRunnable):
+=======
+class MainWindow(QMainWindow):
+>>>>>>> parent of e35213c (Version beta 1.0)
     def __init__(self):
         super().__init__()
-        self.signals = WorkerSignal()
-
-    @pyqtSlot()
-    def run(self):
-        total = 1000
-        for n in range(total):
-            progress = int(100 * float(n + 1) / total)
-            self.signals.progress.emit(progress)
-            time.sleep(0.1)
-
-
-class MainWindow(QMainWindow):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        gestor = QVBoxLayout()
-        self.progreso = QLCDNumber()
-        widget = QWidget()
-        btnIniciar = QPushButton("Iniciar")
-        btnIniciar.clicked.connect(self.ejecutar)
-
-        gestor.addWidget(self.progreso)
-        gestor.addWidget(btnIniciar)
-        widget.setLayout(gestor)
-        self.setCentralWidget(widget)
-        self.threadpool = QThreadPool()
-        print("Multihilos con un maximo %d hilos" % self.threadpool.maxThreadCount())
-
-    def ejecutar(self):
-        worker = Worker()
-        worker.signals.progress.connect(self.actualizarProgreso)
-        self.threadpool.start(worker)
-
-    def actualizarProgreso(self, progress):
-        self.progreso.display(progress)
+        text = QLabel("Bienvenido")
+        self.setCentralWidget(text)
 
 
 app = QApplication(sys.argv)
