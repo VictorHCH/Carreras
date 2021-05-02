8749addf1342dff0e7698193b8ad8a64b1609e72
Hiram Hernandez <pollo2reliptube1@gmail.com>
2021-05-02 12:19:17 -0600

    Version alpha 0.5

--- Carrera/main.py
+++ Carrera/main.py
@@ -16,8 +16,9 @@ class MainWindow(QMainWindow):
                            " border-bottom-right-radius : 6px;"
                            )
         self.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.resize(900, 600)
 
-        self.titulo = QLabel("            Carrera")
+        self.titulo = QLabel("                                                                                                                                       Carrera")
         self.titulo.setFont(QFont("Segoe UI Light", 12))
         self.barra = QToolBar("barra de herramientas")
         self.barra.setIconSize(QSize(16, 16))
@@ -27,6 +28,7 @@ class MainWindow(QMainWindow):
                                  "border-bottom-left-radius: 0px;"
                                  "border-bottom-right-radius: 0px")
 
+ #---------------------------------Barra de tareas----------------------------------#
         boton_accion = QAction(QIcon("Imagenes\Cerrar.png"), "Cerrar", self)
         boton_accion.triggered.connect(self.cerrar)
         self.barra.addAction(boton_accion)
@@ -34,19 +36,44 @@ class MainWindow(QMainWindow):
         boton_accion2 = QAction(QIcon("Imagenes\Minimizar.png"), "Minimizar", self)
         boton_accion2.triggered.connect(self.minimizar)
         self.barra.addAction(boton_accion2)
-
-
         self.barra.setMovable(False)
         self.barra.addWidget(self.titulo)
 
-        text = QLabel("Bienvenido")
-        text2 = QLabel("Esta es una alfa!")
+#------------------------------------------Fondo---------------------------------------------------#
+        pixmap = QPixmap("Imagenes\Banqueta.png")
+        bg = QLabel(self)
+        pixmap = pixmap.scaled(1200, 1200, Qt.KeepAspectRatio, Qt.FastTransformation)
+        bg.setPixmap(pixmap)
+
+
+
         lo = QHBoxLayout()
-        lo.addWidget(text)
-        lo.addWidget(text2)
+        lo.addWidget(bg)
         widget = QWidget()
         widget.setLayout(lo)
         self.setCentralWidget(widget)
+# ------------------------------------------Boton------------------------------------------------#
+   # Esta parte aun no la arreglo, el icono del boton no se asigna bien y no se hace transparente xD
+    
+        #Prueba = QPushButton(self)
+        #botn = QPixmap("Imagenes\Boton.png")
+        #ico = QIcon(botn)
+        #Prueba.setIcon(ico)
+        #Prueba.setAttribute(Qt.WA_TranslucentBackground, True)
+        #Prueba.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        #Prueba.setIconSize(pixmap.rect().size())
+        #Prueba.resize(212, 200)
+        #Prueba.setStyleSheet("border-radius: 5px; color: white; background: rgb(165,165,165); border: 2px solid white;")
+        #Prueba.move(950 / 2 - 150 / 2, 780 / 2 - 40 / 2)
+
+# ------------------------------------------Titulo------------------------------------------------#
+        pixmap = QPixmap("Imagenes\Carrera.png")
+        titulo = QLabel(self)
+        pixmap = pixmap.scaled(1000, 1000, Qt.KeepAspectRatio, Qt.FastTransformation)
+        titulo.setAttribute(Qt.WA_TranslucentBackground, True)
+        titulo.setPixmap(pixmap)
+        titulo.resize(1000, 1000)
+        titulo.move(370 / 2 - 150 / 2, - 540 / 2 - 40 / 2)
 
     def mousePressEvent(self, event):
         self.oldPos = event.globalPos()
