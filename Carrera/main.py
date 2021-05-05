9b9313fea3bffe48373a94b2acae5262bd0173ea
Hiram Hernandez <pollo2reliptube1@gmail.com>
2021-05-05 15:37:31 -0600

    Version beta 3.1
    Se agrego una ventana para mostrar al ganador, aun no es llamable

--- Carrera/main.py
+++ Carrera/main.py
@@ -73,21 +73,17 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
 
 # ------------------------------------------Boton------------------------------------------------#
-   # Esta parte aun no la arreglo, el icono del boton no se asigna bien y no se hace transparente xD
     
-        self.Prueba = QPushButton(self)
-        botn = QPixmap("Imagenes\Boton.png")
-        ico = QIcon(botn)
-        self.Prueba.setIcon(ico)
-        self.Prueba.setAttribute(Qt.WA_TranslucentBackground, True)
-        self.Prueba.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.Prueba.setIconSize(pixmap.rect().size())
-        self.Prueba.resize(212, 200)
-        self.Prueba.setStyleSheet("border-radius: 5px; color: white; background: rgb(165,165,165); border: 2px solid white;")
-        self.Prueba.move(int(1060 / 2 - 150 / 2), int(820 / 2 - 40 / 2))
-        self.Prueba.clicked.connect(self.iniciarCarrera)
+        self.Comenzar = QPushButton("Comenzar! ",self)
+        self.Comenzar.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.Comenzar.setStyleSheet("border-radius: 5px; color: Blue; background:cyan; border: 2px solid white;")
+        self.Comenzar.resize(190, 100)
+        self.Comenzar.move(int(1250 / 2 - 190 / 2), int(820 / 2 - 40 / 2))
+        self.Comenzar.clicked.connect(self.iniciarCarrera)
+        #self.Comenzar.clicked.connect(self.ganador) (Evento para probar la ventana)
 
 # ------------------------------------------Titulo------------------------------------------------#
+
         pixmap = QPixmap("Imagenes\Carrera.png")
         self.titulo = QLabel(self)
         pixmap = pixmap.scaled(1000, 1000, Qt.KeepAspectRatio, Qt.FastTransformation)
@@ -97,6 +93,7 @@ class MainWindow(QMainWindow):
         self.titulo.move(120, 150)
 
 # ------------------------------------------Cronometro------------------------------------------------#
+
         self.lcd = QLCDNumber(self)
         self.lcd.setAttribute(Qt.WA_TranslucentBackground, True)
         self.lcd.display("00:00")
@@ -125,7 +122,7 @@ class MainWindow(QMainWindow):
         self.tortuga.setAttribute(Qt.WA_TranslucentBackground, True)
         self.tortuga.setPixmap(mapTortuga)
         self.tortuga.resize(200, 200)
-        self.tortuga.move(5, 325)
+        self.tortuga.move(5, 310)
         self.tortuga.hide()
 
     def iniciarCarrera(self):
@@ -134,8 +131,8 @@ class MainWindow(QMainWindow):
         self.liebre.show()
         self.tortuga.show()
         self.lcd.show()
-        self.Prueba.hide()
-        self.Prueba.setEnabled(False)
+        self.Comenzar.hide()
+        self.Comenzar.setEnabled(False)
 
     def iniciar_timer(self):
         self.time += 1
@@ -155,6 +152,96 @@ class MainWindow(QMainWindow):
 
     def mouseMoveEvent(self, event):
         delta = QPoint (event.globalPos() - self.oldPos)
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.oldPos = event.globalPos()
+
+    def minimizar(self):
+        self.showMinimized()
+
+    def cerrar(self):
+        self.close()
+
+    def ganador(self):
+        self.pod = podio()
+        self.pod.show()
+
+class podio(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+#----------------------------------------------------Ventana-----------------------------------------------#
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.setStyleSheet("background: rgb(242,242,242,250); color: black; border-top-right-radius: 0px;"
+                           " border-top-right-radius : 0px;"
+                           " border-bottom-left-radius : 6px;"
+                           " border-bottom-right-radius : 6px;"
+                           )
+# ------------------------------------Barra de tareas---------------------------------------#
+        self.titulo = QLabel("Ganador")
+        self.titulo.setFont(QFont("Segoe UI Light", 12))
+        self.barra2 = QToolBar("barra de herramientas")
+        self.barra2.setIconSize(QSize(16, 16))
+        self.addToolBar(self.barra2)
+        self.barra2.setStyleSheet("background: rgb(165,165,165,250); color: white; border-top-right-radius: 8px;"
+                                 "border-top-right-radius: 8px;"
+                                 "border-bottom-left-radius: 0px;"
+                                 "border-bottom-right-radius: 0px")
+        izquierda = QWidget()
+        izquierda.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        derecha = QWidget()
+        derecha.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        boton_accion = QAction(QIcon("Imagenes\Cerrar.png"), "Cerrar", self)
+        boton_accion.triggered.connect(self.cerrar)
+        self.barra2.addAction(boton_accion)
+# ---------------------------------Barra de tareas----------------------------------#
+        boton_accion2 = QAction(QIcon("Imagenes\Minimizar.png"), "Minimizar", self)
+        boton_accion2.triggered.connect(self.minimizar)
+        self.barra2.addAction(boton_accion2)
+        self.barra2.setMovable(False)
+        self.barra2.addWidget(izquierda)
+        self.barra2.addWidget(self.titulo)
+        self.barra2.addWidget(derecha)
+#--------------------------------------------Fondo--------------------------------------#
+        pixmap = QPixmap("Imagenes\podio.png")
+        bg = QLabel(self)
+        pixmap = pixmap.scaled(1200, 1200, Qt.KeepAspectRatio, Qt.FastTransformation)
+        bg.setPixmap(pixmap)
+        lo = QHBoxLayout()
+        lo.addWidget(bg)
+        widget = QWidget()
+        widget.setLayout(lo)
+        self.setCentralWidget(widget)
+
+        #winner = False (Para probar las posiciones)
+#------------------------------------Animales------------------------------------------------------"
+        mapLiebre = QPixmap("Imagenes\Liebre.png")
+        mapLiebre = mapLiebre.scaled(320, 320, Qt.KeepAspectRatio, Qt.FastTransformation)
+        mapTortuga = QPixmap("Imagenes\Tortuga.png")
+        mapTortuga = mapTortuga.scaled(320, 320, Qt.KeepAspectRatio, Qt.FastTransformation)
+#---------------------------------Etiquteas--------------------------------#
+        self.uno = QLabel(self)
+        self.uno.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.uno.resize(320, 320)
+        self.uno.move(295, 215)
+
+        self.dos = QLabel(self)
+        self.dos.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.dos.resize(320, 320)
+        self.dos.move(650, 310)
+#--------------------------Condicional de ganador--------------------------------#
+        if winner == True:
+            self.uno.setPixmap(mapLiebre)
+            self.dos.setPixmap(mapTortuga)
+        else:
+            self.uno.setPixmap(mapTortuga)
+            self.dos.setPixmap(mapLiebre)
+
+    def mousePressEvent(self, event):
+        self.oldPos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        delta = QPoint(event.globalPos() - self.oldPos)
         self.move(self.x() + delta.x(), self.y() + delta.y())
         self.oldPos = event.globalPos()
 
